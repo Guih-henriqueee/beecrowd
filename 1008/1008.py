ec9d4634a215452cca0c69a469ea4686ce5cf6ee
Guih-henriquee <126841778+Guih-henriqueee@users.noreply.github.com>
2023-12-31 05:33:05 -0300

    Desafio 1008 - Accepted

---
+++ 1008/1008.py
@@ -0,0 +1,7 @@
+    id_funcionario = int(input())
+    horas_t = int(input())
+    valorHora = float(input())
+
+    salario = valorHora * horas_t
+
+    print(f'NUMBER = {id_funcionario}\nSALARY = U$ {salario:.2f}')

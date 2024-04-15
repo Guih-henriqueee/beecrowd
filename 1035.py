370728c568c4d9684fcaabef221a3c8958a82dc9
Guilherme Martins <126841778+Guih-henriqueee@users.noreply.github.com>
2024-04-15 13:58:49 -0300

    Update 1035.py

--- 1035.py
+++ 1035.py
@@ -6,10 +6,6 @@ B = int(numeros[1]) # Pega o numero na segunda posição
 C = int(numeros[2]) # Pega o numero na terceira posição
 D = int(numeros[3]) # Pega o numero na quarta posição
 
-
-soma1 = (A + B) # salva a soma de A + B
-soma2 = (C + D) # salva a soma de C + D
-
 msg1 = "Valores aceitos"
 msg2 = "Valores nao aceitos"
 
@@ -18,7 +14,7 @@ if (A % 2 != 0): # Verifica o primeiro critério eliminatório master
     print (msg2)
 
 else:
-    if (D > A) and (B > C) and (soma1 < soma2) and (C > 0 and D > 0): # Lógica aninhada
+    if (D > A) and (B > C) and (( A + B) < (C + D)) and (C > 0 and D > 0): # Lógica aninhada
         print(msg1)
 
     else:

3c3153255784154beb71f2097b5e6de1aaa27efb
Guilherme Martins <126841778+Guih-henriqueee@users.noreply.github.com>
2025-10-15 01:44:07 +0000

    add: Include new lenguage to learn C#
    Rebuild repository to have exercises in C# and Python

---
+++ C#/1009/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace KPISeller
+{
+
+    public class Vendedores
+    {
+
+        public string Vendedor { get; private set; }
+        public double Vendas { get; private set; }
+        public double SalarioBase { get; private set; }
+        public double SalarioFinal { get; private set; }
+        public double BonusPorcentagem = 0.15f;
+
+        public Vendedores(string Seller, double Sales, double Wage)
+        {
+            Vendedor = Seller;
+            Vendas = Sales;
+            SalarioBase = Wage;
+
+            CalcularSalario();
+        }
+
+        public void CalcularSalario()
+        {
+            SalarioFinal = Vendas * BonusPorcentagem;
+            SalarioFinal += SalarioBase; 
+        }
+
+        public double GetSalarioFinal()
+        {
+            return SalarioFinal;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            string Seller = Console.ReadLine();
+            double Wage = double.Parse(Console.ReadLine());
+            double Sales = double.Parse(Console.ReadLine());
+
+            Vendedores pesquisa = new Vendedores(Seller, Sales, Wage);
+            Console.WriteLine($"TOTAL = R$ {pesquisa.GetSalarioFinal():F2}");
+        }
+    }
+}
---
+++ C#/2374/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CalibragemPneus
+{
+    
+    public class Calibrador
+    {
+        
+        public int Pressao { get; private set; }
+        public int PressaoAtual { get; private set;}
+        public int Calibragem { get; private set;}
+    
+        public Calibrador(int p, int pa )
+        {
+            Pressao = p;
+            PressaoAtual = pa;
+            
+            CalcularCalibragem();
+        }
+        
+        
+        private void CalcularCalibragem()
+        {
+            Calibragem = PressaoAtual - Pressao;
+        }
+        
+        public int GetCalibragem()
+        {
+            return Calibragem;
+        }
+    }
+    
+    class Program
+    {
+        
+        static void Main()
+        {
+            int p = int.Parse(Console.ReadLine());
+            int pa = int.Parse(Console.ReadLine());
+        
+            Calibrador pesquisa = new Calibrador(p, pa);
+            Console.WriteLine(pesquisa.GetCalibragem());
+        }
+    }
+    
+}
